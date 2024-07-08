5324dda14e1ba69807491265ae83b98f972ff7f0
FediMechergui <fedimechergui03@gmail.com>
2024-07-08 09:45:33 +0100

    Initial commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://technotes.netlify.app',
+    'https://technotes.vercel.app',
+    'https://technotes.coolify.app'
+]
+
+module.exports = allowedOrigins
